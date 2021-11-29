21b94b044f026bbfa500bf7f93c2a0856b066edf
igor <xdentus@outlook.com>
2021-11-30 00:32:50 +0100

    ui completed, functions not working

--- javascript.js
+++ javascript.js
@@ -1,3 +1,40 @@
-function TextChange(){
-    document.getElementById("demo").innerHTML = 5 + 6;
+function ChangeToOne(){
+    b = a.toString() + 1;
+    document.getElementById("equation").innerHTML = b;
+}
+function ChangeToTwo(){
+    b = a.toString() + 2;
+    document.getElementById("equation").innerHTML = b;
+}
+function ChangeToThree(){
+    a = 3;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToFour(){
+    a = 4;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToFive(){
+    a = 5;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToSix(){
+    a = 6;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToSeven(){
+    a = 7;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToEight(){
+    a = 8;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToNine(){
+    a = 9;
+    document.getElementById("equation").innerHTML = a;
+}
+function ChangeToZero(){
+    a = 0;
+    document.getElementById("equation").innerHTML = a;
 }
